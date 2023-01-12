f550ad15879ead527b9e306d16e392fe39b1e715
SurendraVSS <106951672+SurendraVSS@users.noreply.github.com>
2023-01-12 20:05:15 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -8,52 +8,59 @@ const API_KEYS = '77f0958748cc0c5e0cf89f9bfa0d3a6f'
 const Weather = () => {
     const [city, setCity] = useState(null)
     const [weatherData, setWeatherData] = useState(null)
-   async function getWeatherData(city){
-       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=77f0958748cc0c5e0cf89f9bfa0d3a6f&units=metric`)
-      // console.log(response);
-      if(response.status == 200){
-          const data = await response.json()
-          setWeatherData(data);
-      }else{
-        setWeatherData(null)
-      }
-      
-      
-      
+    const [error, setError] = useState(null)
+    async function getWeatherData(city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=77f0958748cc0c5e0cf89f9bfa0d3a6f&units=metric`)
+            .then(response => response.json())
+            .then(response => {
+                
+                if (response.cod == 200) {
+                    setWeatherData(response);
+                } else {
+                    setWeatherData(null)
+                    setError(response.message)
+                }
+
+                setCity(null)
+
+            }).catch(err => console.log(err))
+        // 
     }
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-               <TextInput
-               style={{backgroundColor:'pink',width:300,height:40, borderRadius:10}}
-               value={city}
-               onChangeText={(text) => setCity(text)}
-               placeholder="Enter city name"
-               ></TextInput>
-               <Button 
-                onPress={() => getWeatherData(city)}
-                title="Search"
+                <TextInput
+                    style={{ backgroundColor: 'pink', width: 300, height: 40, borderRadius: 10 }}
+                    value={city}
+                    onChangeText={(text) => setCity(text)}
+                    placeholder="Enter city name"
+                ></TextInput>
+                <Button
+                    onPress={() => getWeatherData(city)}
+                    title="Search"
                     color="#841584"
-               />
+                />
             </View>
-            {weatherData != null ? <View>
-                <Text> Weather Data of {city}</Text>
+            {weatherData == null ? <><View><Text>
+                {error}
+            </Text></View></> : <View>
+                <Text> Weather Data of {weatherData?.name}</Text>
                 <Text>{weatherData?.main.humidity}</Text>
                 <Text>{weatherData?.main.temp}</Text>
                 <Text>{weatherData?.weather[0].description}</Text>
-            </View>: null}
-            
+            </View>}
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-       marginTop:100,
+        marginTop: 100,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-    },  
+    },
 })
 
 export default Weather;
